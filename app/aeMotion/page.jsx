7486c9ba27df6f92539906c9b3505dca7734cc3f
Malik 3 <malikkotb@icloud.com>
2024-12-15 11:56:28 +0100

    ae motion

--- app/aeMotion/page.jsx
+++ app/aeMotion/page.jsx
@@ -5,25 +5,57 @@ import gsap from "gsap";
 import styles from "./style.module.css";
 
 const AeMotionPage = () => {
-  const divRef1 = useRef(null);
-  const divRef2 = useRef(null);
+  const leftTextRef = useRef(null);
+  const rightTextRef = useRef(null);
   const imgRef = useRef(null);
 
   const masterTl = gsap.timeline();
   useGSAP(() => {
-    gsap.set(imgRef.current, { scale: 0.05 });
+    gsap.set(imgRef.current, { scale: 0 });
 
-    masterTl.to(imgRef.current, { scale: 1, duration: 2 });
+    // TODO: add easings
+
+    masterTl
+      .to(imgRef.current, { visibility: "visible" })
+      .to(imgRef.current, { scale: 1, x: -125, duration: 0.4 }, "<")
+
+      .to(leftTextRef.current, { x: -60, y: -265, duration: 0.4 }, "<")
+      .to(rightTextRef.current, { x: 75, y: 265, duration: 0.4 }, "<")
+      .add(() => {
+        if (imgRef.current) {
+          imgRef.current.src = "/img2.png";
+        }
+      }, "-=0.3")
+      .add(() => {
+        if (leftTextRef.current) {
+          leftTextRef.current.textContent = "New Text!";
+        }
+      }, "-=0.3")
+      .add(() => {
+        if (imgRef.current) {
+          imgRef.current.src = "/img2.png";
+        }
+      }, "-=0.3")
+      .to(imgRef.current, { scale: 0, duration: 0.4 });
   }, []);
 
   return (
-    <div className="h-screen grid place-content-center bg-[#ddd]">
-      <div className="h-screen w-full absolute grid place-content-center">
-        <img ref={imgRef} src="/img1.png" alt="img" className="" />
+    <div className="h-screen grid place-content-center bg-[#ddd] overflow-hidden">
+      <div className="h-screen w-full absolute grid place-content-center overflow-hidden">
+        <img
+          ref={imgRef}
+          src="/img1.png"
+          alt="img"
+          className="h-[40vh] invisible"
+        />
       </div>
       <div className="flex">
-        <h1 className={`${styles.heading}`}>Fa</h1>
-        <h1 className={`${styles.heading}`}>shion</h1>
+        <h1 ref={leftTextRef} className={`${styles.heading}`}>
+          Fa
+        </h1>
+        <h1 ref={rightTextRef} className={`${styles.heading}`}>
+          shion
+        </h1>
       </div>
     </div>
   );
