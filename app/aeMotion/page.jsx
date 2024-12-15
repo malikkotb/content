50b6b1f3b3416772152f872d85cf5f059f2bf62c
Malik 3 <malikkotb@icloud.com>
2024-12-15 01:06:26 +0100

    ae motion

--- app/aeMotion/page.jsx
+++ app/aeMotion/page.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+"use client";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import gsap from "gsap";
+import styles from "./style.module.css";
 
 const AeMotionPage = () => {
-    return (
-        <div>
-            <h1>Welcome to AeMotion</h1>
-            <p>This is a basic React template.</p>
-        </div>
-    );
+  const divRef1 = useRef(null);
+  const divRef2 = useRef(null);
+  const imgRef = useRef(null);
+
+  const masterTl = gsap.timeline();
+  useGSAP(() => {
+    masterTl.to(divRef1.current, { x: 100, duration: 2 });
+  }, []);
+
+  return (
+    <div className="h-screen grid place-content-center bg-[#ddd]">
+      <div className="flex">
+        <h1 className={`${styles.heading}`}>Fa</h1>
+        <h1 className={`${styles.heading}`}>shion</h1>
+      </div>
+    </div>
+  );
 };
 
 export default AeMotionPage;
