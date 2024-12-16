a0e74e746ac879192e0d33eb9e1577db50643944
Malik 3 <malikkotb@icloud.com>
2024-12-16 15:53:08 +0100

    stairs intro

--- app/stairsIntro/page.jsx
+++ app/stairsIntro/page.jsx
@@ -9,10 +9,12 @@ export default function FontWeightAnimation() {
 
   useGSAP(() => {
     const timeLine = gsap.timeline();
-    timeLine.to([stairRefs.current], {
-        scaleY: 0.5,
-        stagger: 0.1
-
+    timeLine.to(stairRefs.current, {
+      scaleY: 0,
+      stagger: 0.1,
+      duration: 1,
+      ease: "power2.inOut",
+      delay: 2,
     });
   }, []);
 
@@ -25,6 +27,14 @@ export default function FontWeightAnimation() {
           className={styles.stairSection}
         ></div>
       ))}
+      {/* TODO: animate only die zehner stelle von den 3 zahlen
+      und dann am ende noch den 100 -> und dann die stairs animation
+      */}
+      <div className="absolute flex w-full bottom-0">
+        <div className={styles.number}>0</div>
+        <div className={styles.number}>0</div>
+        <div className={styles.number}>0</div>
+      </div>
     </div>
   );
 }
