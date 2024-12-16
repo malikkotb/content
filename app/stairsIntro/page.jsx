930e4a8ddd0801dd0a26c47dd9075917ff55e5d3
Malik 3 <malikkotb@icloud.com>
2024-12-16 15:43:22 +0100

    stairs intro

--- app/stairsIntro/page.jsx
+++ app/stairsIntro/page.jsx
@@ -1,18 +1,30 @@
 "use client";
 import { useRef } from "react";
 import { gsap } from "gsap";
+import styles from "./style.module.css";
 import { useGSAP } from "@gsap/react";
 
 export default function FontWeightAnimation() {
-  const textRef = useRef(null);
+  const stairRefs = useRef([]);
 
   useGSAP(() => {
+    const timeLine = gsap.timeline();
+    timeLine.to([stairRefs.current], {
+        scaleY: 0.5,
+        stagger: 0.1
 
-  }, [])
+    });
+  }, []);
 
   return (
-    <div className="">
-      
+    <div className="flex">
+      {[...Array(4)].map((_, index) => (
+        <div
+          key={index}
+          ref={(el) => (stairRefs.current[index] = el)} // Assign each element to the array
+          className={styles.stairSection}
+        ></div>
+      ))}
     </div>
   );
 }
