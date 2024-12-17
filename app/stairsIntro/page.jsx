ac930c2d061b630bf0d086f20a67ca66359f5aed
Malik 3 <malikkotb@icloud.com>
2024-12-17 11:30:41 +0100

    stairs intro

--- app/stairsIntro/page.jsx
+++ app/stairsIntro/page.jsx
@@ -1,8 +1,19 @@
 "use client";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import styles from "./style.module.css";
 import { useGSAP } from "@gsap/react";
+import localFont from "next/font/local";
+
+const stepsMonoFont = localFont({
+  src: [
+    {
+      path: "../../app/fonts/Steps-Mono.otf",
+      weight: "700",
+      style: "bold",
+    },
+  ],
+});
 
 export default function FontWeightAnimation() {
   const stairRefs = useRef([]);
@@ -13,8 +24,9 @@ export default function FontWeightAnimation() {
       refArray.current.push(el);
     }
   };
-
   const addToNumberRefs = addToRefs(numberRefs);
+
+  useEffect(() => {}, []);
 
   useGSAP(() => {
     const timeLine = gsap.timeline();
@@ -24,7 +36,7 @@ export default function FontWeightAnimation() {
         stagger: 0.1,
         duration: 1,
         ease: "power2.inOut",
-        delay: 2,
+        delay: 1.5,
       })
 
       .to(
@@ -40,7 +52,7 @@ export default function FontWeightAnimation() {
   }, []);
 
   return (
-    <div className="flex">
+    <div className={`flex ${stepsMonoFont.className}`}>
       {[...Array(4)].map((_, index) => (
         <div
           key={index}
@@ -51,7 +63,13 @@ export default function FontWeightAnimation() {
       {/* TODO: animate only die zehner stelle von den 3 zahlen
       und dann am ende noch den 100 -> und dann die stairs animation
       */}
-      <div className="absolute flex w-full borderr font-mono bottom-0">
+      <div className="fixed flex flex-col justify-center items-center text-[350px] text-[#ff7c23] h-screen w-full borderr">
+        <div className="flex-col flex">
+          <span className="tracking-tighter leading-[250px]">Hello</span>
+          <span className="text-2xl ml-4">Have a nice day.</span>
+        </div>
+      </div>
+      <div className={`absolute flex w-full bottom-0`}>
         <div ref={addToNumberRefs} className={styles.number}>
           0
         </div>
