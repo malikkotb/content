ba66d8cd0f3257a2b6d14e605852ff544e150451
Malik 3 <malikkotb@icloud.com>
2024-12-17 11:46:56 +0100

    stairs intro

--- app/stairsIntro/page.jsx
+++ app/stairsIntro/page.jsx
@@ -26,7 +26,35 @@ export default function FontWeightAnimation() {
   };
   const addToNumberRefs = addToRefs(numberRefs);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    numberRefs.current.forEach((numberRef, index) => {
+      if (index === 0) {
+        setTimeout(() => {
+          numberRef.textContent = 1;
+        }, 2000);
+      }
+      if (index === 1) {
+        let count = 0;
+
+        const interval = setInterval(() => {
+          numberRef.textContent = count;
+
+          if (count === 9) {
+            clearInterval(interval);
+
+            // Delay before setting the content to "0"
+            setTimeout(() => {
+              numberRef.textContent = 0;
+            }, 500);
+          }
+
+          count++;
+        }, 150 * (count + 1));
+
+        return () => clearInterval(interval); // Ensure cleanup in case of component unmount
+      }
+    });
+  }, []);
 
   useGSAP(() => {
     const timeLine = gsap.timeline();
@@ -36,7 +64,7 @@ export default function FontWeightAnimation() {
         stagger: 0.1,
         duration: 1,
         ease: "power2.inOut",
-        delay: 1.5,
+        delay: 2.3,
       })
 
       .to(
@@ -51,6 +79,8 @@ export default function FontWeightAnimation() {
       );
   }, []);
 
+  // TODO: caption: i think loading screens should not be too long
+
   return (
     <div className={`flex ${stepsMonoFont.className}`}>
       {[...Array(4)].map((_, index) => (
@@ -63,10 +93,10 @@ export default function FontWeightAnimation() {
       {/* TODO: animate only die zehner stelle von den 3 zahlen
       und dann am ende noch den 100 -> und dann die stairs animation
       */}
-      <div className="fixed flex flex-col justify-center items-center text-[350px] text-[#ff7c23] h-screen w-full borderr">
-        <div className="flex-col flex">
+      <div className="fixed flex flex-col justify-center items-center text-[350px] text-[#ff7c23] h-screen w-full">
+        <div className="flex-col flex ">
           <span className="tracking-tighter leading-[250px]">Hello</span>
-          <span className="text-2xl ml-4">Have a nice day.</span>
+          <span className="text-4xl ml-4">Have a nice day.</span>
         </div>
       </div>
       <div className={`absolute flex w-full bottom-0`}>
