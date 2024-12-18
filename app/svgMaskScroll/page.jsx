09c388c61381be9b72ae6f82c077e26389208152
Malik 3 <malikkotb@icloud.com>
2024-12-18 20:32:43 +0100

    font weight animation

---
+++ app/svgMaskScroll/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import { useRef } from "react";
+import { gsap } from "gsap";
+
+export default function SvgMaskScrollPage() {
+  //  TODO: add lenis scroll here to make it smoother
+  // use videos that are revealed with mask
+  return (
+    <div className={`bg-[#ddd] h-screen flex flex-col justify-between`}></div>
+  );
+}
