ad5fb3be6e421234d8162b6377f662ac9b097a17
Malik 3 <malikkotb@icloud.com>
2024-12-19 12:27:16 +0100

    font weight animation

--- app/fontWeightHover/page.jsx
+++ app/fontWeightHover/page.jsx
@@ -13,16 +13,6 @@ const neueRegrade = localFont({
 });
 
 export default function FontWeightAnimation() {
-  const textRef = useRef(null);
-
-  const handleMouseEnter = () => {
-    gsap.to(textRef.current, { fontWeight: 700, duration: 0.3 });
-  };
-
-  const handleMouseLeave = () => {
-    gsap.to(textRef.current, { fontWeight: 400, duration: 0.3 });
-  };
-
   const containerRef = useRef(null);
   const hoverAreaRef = useRef(null);
 
@@ -54,7 +44,7 @@ export default function FontWeightAnimation() {
         if (distance < hoverRadius) {
           // Proximity-based font-weight animation
           const proximity = 1 - distance / hoverRadius;
-          const fontWeight = 300 + proximity * 300; // Font weight ranges from 300 to 700
+          const fontWeight = 400 + proximity * 200; // Font weight ranges from 300 to 700
 
           gsap.to(span, {
             fontVariationSettings: `'wght' ${fontWeight}`,
@@ -64,7 +54,7 @@ export default function FontWeightAnimation() {
         } else {
           // Reset font weight when outside the hover area
           gsap.to(span, {
-            fontVariationSettings: `'wght' 300`,
+            fontVariationSettings: `'wght' 400`,
             duration: 0.5,
             ease: "power4.out",
           });
@@ -83,48 +73,34 @@ export default function FontWeightAnimation() {
     <>
       <div
         ref={containerRef}
-        className="flex uppercase h-screen text-[260px] bg-[#FF8400] text-[#1e1e1e] flex-col"
+        className={`flex ${neueRegrade.className} uppercase h-screen text-[260px] bg-[#FF8400] text-[#1e1e1e] flex-col`}
       >
-        <div
+        {/* <div
           ref={hoverAreaRef}
-          className="absolute top-0 left-0 w-[300px] h-[300px] pointer-events-none border border-[#49ff1c] rounded-full opacity-70"
-        ></div>
-        <div className="flex font-extralight leading-[210px]">
+          className="absolute bg-white top-0 left-0 w-[50px] h-[50px] pointer-events-none border mix-blend-difference rounded-full"
+        ></div> */}
+        <div className="flex mt-10 leading-[230px]">
           <span>S</span>
           <span>E</span>
           <span>P</span>
         </div>
-        <div className="flex justify-end leading-[210px]">
+        <div className="flex justify-end leading-[230px]">
           <span>T</span>
           <span>E</span>
           <span>M</span>
         </div>
-        <div className="flex font-bold justify-center leading-[210px]">
+        <div className="flex justify-center leading-[230px]">
           <span>B</span>
           <span>E</span>
           <span>R</span>
         </div>
-        <div className="flex justify-center leading-[210px]">
+        <div className="flex justify-center leading-[230px]">
           <span>2</span>
-          <span className=" italic">0</span>
+          <span className="">0</span>
           <span>2</span>
           <span>1</span>
           <span>©</span>
         </div>
-      </div>
-      <div
-        className={`bg-[#ddd] h-screen ${neueRegrade.className} flex flex-col justify-between`}
-      >
-        <h1 className="text-4xl font-bold text-center borderr flex-1">
-          <span
-            ref={textRef}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            className="borderr w-fit uppercase"
-          >
-            Hover Over Me!
-          </span>
-        </h1>
       </div>
     </>
   );
