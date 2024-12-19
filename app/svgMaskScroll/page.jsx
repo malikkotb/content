828ead92b11200d6bca20ed3566420d0f162ab9d
Malik 3 <malikkotb@icloud.com>
2024-12-19 16:07:02 +0100

    mask reveal

--- app/svgMaskScroll/page.jsx
+++ app/svgMaskScroll/page.jsx
@@ -1,11 +1,94 @@
 "use client";
-import { useRef } from "react";
-import { gsap } from "gsap";
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import styles from "./page.module.css";
 
-export default function SvgMaskScrollPage() {
-  //  TODO: add lenis scroll to this page here to make it smoother
-  // use videos that are revealed with mask
+export default function Home() {
+  const container = useRef(null);
+  const images = useRef([]);
+
+  useEffect(() => {
+    const totalScrollDistance = 150 * images.current.length; // 150vh per image
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: container.current,
+        start: "top top",
+        end: `${totalScrollDistance}vh`,
+        scrub: true,
+      },
+    });
+
+    images.current.forEach((image, index) => {
+      tl.to(
+        image,
+        {
+          maskSize: "400%",
+          webkitMaskSize: "400%",
+          duration: 1,
+          ease: "none",
+        },
+        index // Stagger animations for each image
+      );
+    });
+  }, []);
+
   return (
-    <div className={`bg-[#ddd] h-screen flex flex-col justify-between`}></div>
+    <main className={styles.main}>
+      <div ref={container} className={styles.container}>
+        {[...Array(3)].map((_, i) => (
+          <div
+            key={i}
+            ref={(el) => (images.current[i] = el)}
+            className={styles.imageWrapper}
+          >
+            <img src={`/medias/img${i + 1}.png`} alt={`Image ${i + 1}`} />
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
+
+// "use client";
+// import { useRef, useEffect } from "react";
+// import styles from "./page.module.css";
+
+// export default function Home() {
+//   const container = useRef(null);
+//   const stickyMask = useRef(null);
+
+//   const initialMaskSize = 0;
+//   const targetMaskSize = 30;
+//   const easing = 0.15;
+//   let easedScrollProgress = 0;
+
+//   useEffect(() => {
+//     requestAnimationFrame(animate);
+//   }, []);
+
+//   const animate = () => {
+//     const maskSizeProgress = targetMaskSize * getScrollProgress();
+//     stickyMask.current.style.webkitMaskSize =
+//       (initialMaskSize + maskSizeProgress) * 100 + "%";
+//     requestAnimationFrame(animate);
+//   };
+
+//   const getScrollProgress = () => {
+//     const scrollProgress =
+//       stickyMask.current.offsetTop /
+//       (container.current.getBoundingClientRect().height - window.innerHeight);
+//     const delta = scrollProgress - easedScrollProgress;
+//     easedScrollProgress += delta * easing;
+//     return easedScrollProgress;
+//   };
+
+//   return (
+//     <main className={styles.main}>
+//       <div ref={container} className={styles.container}>
+//         <div ref={stickyMask} className={styles.stickyMask}>
+//           <img src="/medias/img1.png" alt="landscape img" />
+//         </div>
+//       </div>
+//     </main>
+//   );
+// }
