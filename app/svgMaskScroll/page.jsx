96a52c87b3031ff8f3ced8ef3247873a6befb626
Malik 3 <malikkotb@icloud.com>
2024-12-19 16:06:08 +0100

    mask reveal

--- app/svgMaskScroll/page.jsx
+++ app/svgMaskScroll/page.jsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import { gsap } from "gsap";
 
 export default function SvgMaskScrollPage() {
-  //  TODO: add lenis scroll here to make it smoother
+  //  TODO: add lenis scroll to this page here to make it smoother
   // use videos that are revealed with mask
   return (
     <div className={`bg-[#ddd] h-screen flex flex-col justify-between`}></div>
