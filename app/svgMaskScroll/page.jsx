719d645d6737b2ba56c8b93a6cc7941f1ebea827
Malik 3 <malikkotb@icloud.com>
2024-12-20 16:38:18 +0100

    remove

--- app/svgMaskScroll/page.jsx
+++ app/svgMaskScroll/page.jsx
@@ -43,58 +43,6 @@ export default function Home() {
   );
 }
 
-
-// "use client";
-// import { useRef, useEffect } from "react";
-// import gsap from "gsap";
-// import styles from "./style.module.css";
-
-// export default function Home() {
-//   const container = useRef(null);
-//   const images = useRef([]);
-
-//   useEffect(() => {
-//     const totalScrollDistance = 150 * images.current.length; // 150vh per image
-//     const tl = gsap.timeline({
-//       scrollTrigger: {
-//         trigger: container.current,
-//         start: "top top",
-//         end: `${totalScrollDistance}vh`,
-//         scrub: true,
-//       },
-//     });
-
-//     images.current.forEach((image, index) => {
-//       tl.to(
-//         image,
-//         {
-//           maskSize: "400%",
-//           webkitMaskSize: "400%",
-//           duration: 1,
-//           ease: "none",
-//         },
-//         index // Stagger animations for each image
-//       );
-//     });
-//   }, []);
-
-//   return (
-//     <main className={styles.main}>
-//       <div ref={container} className={styles.container}>
-//         {[...Array(3)].map((_, i) => (
-//           <div
-//             key={i}
-//             ref={(el) => (images.current[i] = el)}
-//             className={styles.imageWrapper}
-//           >
-//             <img src={`/maskReveal/img${i + 1}.png`} alt={`Image ${i + 1}`} />
-//           </div>
-//         ))}
-//       </div>
-//     </main>
-//   );
-// }
-
 // "use client";
 // import { useRef, useEffect } from "react";
 // import styles from "./page.module.css";
