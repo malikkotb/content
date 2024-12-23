6fbcc4405f5c40a4f9e628fad43b5fc7d1747675
Malik 3 <malikkotb@icloud.com>
2024-12-23 13:46:30 +0100

    drag

--- app/drag/page.jsx
+++ app/drag/page.jsx
@@ -12,10 +12,19 @@ gsap.registerPlugin(Draggable);
 export default function Home() {
   const containerRef = useRef(null);
   const img1Ref = useRef(null);
+  const img2Ref = useRef(null);
+  const img3Ref = useRef(null);
+  const img4Ref = useRef(null);
+  const img5Ref = useRef(null);
+  const img6Ref = useRef(null);
+  const img7Ref = useRef(null);
   useGSAP(() => {
     Draggable.create(img1Ref.current, {
       bounds: containerRef.current,
-      inertia: true,
+    });
+
+    Draggable.create(img2Ref.current, {
+      type: "rotation",
     });
   }, []);
 
@@ -30,10 +39,76 @@ export default function Home() {
       <section ref={containerRef} className="h-screen w-full">
         <div
           ref={img1Ref}
-          className="w-52 h-52 top-52 left-52 relative rounded-xl"
+          className="w-52 h-60 top-52 left-52 relative rounded-xl"
         >
           <Image
             src="/img1.png"
+            fill
+            className="object-cover rounded-xl"
+            alt="1"
+          />
+        </div>
+        <div
+          ref={img2Ref}
+          className="w-40 h-64 top-20 left-20 relative rounded-xl"
+        >
+          <Image
+            src="/img6.png"
+            fill
+            className="object-cover rounded-xl"
+            alt="1"
+          />
+        </div>
+        <div
+          ref={img3Ref}
+          className="w-36 h-72 top-40 left-72 relative rounded-xl"
+        >
+          <Image
+            src="/img3.png"
+            fill
+            className="object-cover rounded-xl"
+            alt="1"
+          />
+        </div>
+        <div
+          ref={img4Ref}
+          className="w-48 h-80 top-80 left-36 relative rounded-xl"
+        >
+          <Image
+            src="/img4.png"
+            fill
+            className="object-cover rounded-xl"
+            alt="1"
+          />
+        </div>
+        <div
+          ref={img5Ref}
+          className="w-32 h-60 top-[25vh] left-[55vw] relative rounded-xl"
+        >
+          <Image
+            src="/img5.png"
+            fill
+            className="object-cover rounded-xl"
+            alt="1"
+          />
+        </div>
+        <div
+          ref={img6Ref}
+          className="w-40 h-72 top-[50vh] left-28 relative rounded-xl"
+        >
+          <Image
+            src="/img7.png"
+            fill
+            className="object-cover rounded-xl"
+            alt="1"
+          />
+        </div>
+        <div
+          ref={img7Ref}
+          className="w-36 h-68 top-[65vh] left-[65vw] relative rounded-xl"
+        >
+          <Image
+            src="/img8.png"
             fill
             className="object-cover rounded-xl"
             alt="1"
