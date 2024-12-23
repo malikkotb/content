3fbca2e32b04e948223bdd2029aaeeab9a5d7dda
Malik 3 <malikkotb@icloud.com>
2024-12-23 13:30:15 +0100

    drag

--- app/drag/page.jsx
+++ app/drag/page.jsx
@@ -2,13 +2,47 @@
 import { useRef, useEffect } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
-import styles from "./style.module.css";
+import Draggable from "gsap/Draggable";
+// import styles from "./style.module.css";
 import { useGSAP } from "@gsap/react";
+import Image from "next/image";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(Draggable);
 
 export default function Home() {
-  useGSAP(() => {}, []);
+  const containerRef = useRef(null);
+  const img1Ref = useRef(null);
+  useGSAP(() => {
+    Draggable.create(img1Ref.current, {
+      bounds: containerRef.current,
+      inertia: true,
+    });
+  }, []);
 
-  return <main className=""></main>;
+  return (
+    <main className="h-screen">
+      <header
+        style={{ zIndex: 100 }}
+        className="fixed top-0 p-3 w-full borderr"
+      >
+        hi
+      </header>
+      <section ref={containerRef} className="h-screen w-full">
+        <div
+          ref={img1Ref}
+          className="w-52 h-52 top-52 left-52 relative rounded-xl"
+        >
+          <Image
+            src="/img1.png"
+            fill
+            className="object-cover rounded-xl"
+            alt="1"
+          />
+        </div>
+      </section>
+      <footer style={{ zIndex: 100 }} className="fixed bottom-0 p-3 w-full">
+        hello@mail.com
+      </footer>
+    </main>
+  );
 }
