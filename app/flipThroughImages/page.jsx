6531ac8e19868ee0e239c986d8fd6141333101d8
Malik 3 <malikkotb@icloud.com>
2024-12-30 14:30:14 +0100

    flip through

--- app/flipThroughImages/page.jsx
+++ app/flipThroughImages/page.jsx
@@ -1,9 +1,20 @@
+"use client";
 import Image from "next/image";
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Lenis from "lenis";
+import { useGSAP } from "@gsap/react";
 
 export default function FlipThroughImages() {
-
-
-    
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
 
   return (
     <div className="h-screen w-full relative overflow-hidden bg-red-200 flex justify-center items-center">
