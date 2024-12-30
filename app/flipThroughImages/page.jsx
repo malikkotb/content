7f935b845584d84355ff7aad67997bbf8ff6468d
Malik 3 <malikkotb@icloud.com>
2024-12-30 14:28:32 +0100

    flip through

--- app/flipThroughImages/page.jsx
+++ app/flipThroughImages/page.jsx
@@ -1,14 +1,30 @@
 import Image from "next/image";
 
 export default function FlipThroughImages() {
+
+
+    
+
   return (
-    <div className="h-screen w-full overflow-hidden bg-red-200">
-      {Array(8).map((image, index) => (
-        <div key={index} className="relative h-56 w-56">
-          {image}
-          <Image src={`/drag/img1.png`} alt="img" fill />
-        </div>
-      ))}
+    <div className="h-screen w-full relative overflow-hidden bg-red-200 flex justify-center items-center">
+      <div className="flex z-10 flex-col gap-1">
+        {Array.from({ length: 8 }, (_, index) => (
+          <div key={index} className="relative h-20 w-16">
+            <Image
+              src={`/drag/img${index + 1}.png`}
+              alt={`img ${index + 1}`}
+              fill
+              className="object-cover"
+            />
+          </div>
+        ))}
+      </div>
+      <Image
+        src={`/drag/img${1}.png`}
+        alt={`img ${1}`}
+        fill
+        className="object-cover"
+      />
     </div>
   );
 }
