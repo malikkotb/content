b960385762fe62b72046c67e19b4a8c292a20163
Malik 3 <malikkotb@icloud.com>
2024-12-30 14:23:53 +0100

    flip through

--- app/flipThroughImages/page.jsx
+++ app/flipThroughImages/page.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import Image from "next/image";
 
 export default function FlipThroughImages() {
   return (
-    <div>
-      <h1>Flip Through Images</h1>
-      {/* Add your image flipping logic here */}
+    <div className="h-screen w-full overflow-hidden bg-red-200">
+      {Array(8).map((image, index) => (
+        <div key={index} className="relative h-56 w-56">
+          {image}
+          <Image src={`/drag/img1.png`} alt="img" fill />
+        </div>
+      ))}
     </div>
   );
 }
