c9d77f685a493316ecc6ea65d86716100205541a
Malik 3 <malikkotb@icloud.com>
2024-12-30 14:15:09 +0100

    flip through

---
+++ app/flipThroughImages/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FlipThroughImages() {
+  return (
+    <div>
+      <h1>Flip Through Images</h1>
+      {/* Add your image flipping logic here */}
+    </div>
+  );
+}
