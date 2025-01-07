1c24ea37166e3e786e1b669f990300b20d8fcc8c
Malik 3 <malikkotb@icloud.com>
2025-01-07 13:32:48 +0100

    adjustments text scrolly

--- app/textScrolly/page.jsx
+++ app/textScrolly/page.jsx
@@ -47,19 +47,19 @@ export default function TextScrolly() {
       },
     });
     gsap.to(svgRef.current, {
-      scale: 25,
-    //   opacity: 0,
+      scale: 10,
+      opacity: 0,
       scrollTrigger: {
         trigger: ".scaleContainer",
         start: "top bottom", // Start when the top of the container reaches the bottom of the viewport
-        end: "bottom top", // End when the bottom of the container reaches the bottom of the viewport
+        end: "bottom bottom", // End when the bottom of the container reaches the bottom of the viewport
         scrub: true,
       },
     });
   }, []);
 
   return (
-    <div className={`${gambarino.className} flex flex-col`}>
+    <div className={`${gambarino.className} flex flex-col text-white bg-black`}>
       <div className="container h-[900vh] flex justify-center items-start mt-40">
         <svg
           ref={svgRef}
@@ -81,9 +81,11 @@ export default function TextScrolly() {
           <text>
             <textPath
               xlinkHref="#a"
-              startOffset="0%"
+              startOffset="0.5%"
               alignmentBaseline="middle"
-              className="text-[212px] tracking-wider"
+              stroke="white"
+              fill="white"
+              className="text-[212px] tracking-wider "
             >
               A turn around in the{" "}
               <tspan alignmentBaseline="middle" className="italic">
@@ -102,7 +104,8 @@ export default function TextScrolly() {
           <path
             ref={pathRef}
             d="M-.3 331c561.7-488.8 1413.2-429.7 1902 132 391 449.3 343.7 1130.6-105.6 1521.6a862.8 862.8 0 0 1-1217.3-84.5 690.2 690.2 0 0 1 67.6-973.8 552.2 552.2 0 0 1 779.1 54.1 441.6 441.6 0 0 1-43.3 623.2 353.5 353.5 0 0 1-498.6-34.6 282.7 282.7 0 0 1 27.7-398.9 226.2 226.2 0 0 1 319.1 22.1 181 181 0 0 1-17.7 255.3"
-            stroke="white"
+            stroke="black"
+            // strokeOpacity={0.8}
             strokeWidth="350px"
             fill="none"
             style={{
@@ -113,7 +116,7 @@ export default function TextScrolly() {
         </svg>
       </div>
 
-      <div className="scaleContainer h-[700vh] w-full"></div>
+      <div className="scaleContainer h-[500vh] w-full"></div>
     </div>
   );
 }
