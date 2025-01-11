9fcbeba16daa7643ff80804051ee024bc04b9870
Malik 3 <malikkotb@icloud.com>
2025-01-11 17:58:45 +0100

    infinte scroll

--- app/infinityScroll/page.jsx
+++ app/infinityScroll/page.jsx
@@ -3,8 +3,8 @@ import gsap from "gsap";
 import styles from "./style.module.css";
 export default function InfinityScroll() {
   return (
-    <div className="">
-      <div className="cardContainer">
+    <div className="mt-20">
+      <div className={styles.cardContainer}>
         <div className={styles.card}>This is the first card.</div>
         <div className={styles.card}>This is a card.</div>
         <div className={styles.card}>This is a card.</div>
