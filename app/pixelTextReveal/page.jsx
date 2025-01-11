53e12f8810d8cfc0a6f5bded04b05db2273acbb7
Malik 3 <malikkotb@icloud.com>
2025-01-11 15:18:35 +0100

    pixel image text reveal

--- app/pixelTextReveal/page.jsx
+++ app/pixelTextReveal/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import Image from "next/image";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
@@ -13,9 +13,18 @@ export default function Page() {
 
   return (
     <div className="bg-black h-screen flex items-center justify-center">
-      <div className="borderr relative h-[35vh] w-[35vh] bg-red-700 grid grid-cols-6 grid-rows-6">
+      <div className="relative h-[35vh] w-[35vh] bg-red-700 grid grid-cols-6 grid-rows-6">
+        <div className="absolute h-full w-full bg-blue-500">
+          <Image
+            src={"/maskReveal/img1.png"}
+            // src={"/maskReveal/img5.png"}
+            fill
+            className="object-cover"
+            alt=""
+          />
+        </div>
         {Array.from({ length: 36 }).map((_, index) => (
-          <div key={index} className="bg-white borderr"></div>
+          <div key={index} className="bg-orange-400 z-10 opacity-0"></div>
         ))}
       </div>
     </div>
