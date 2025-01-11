edf75dca66ee4d58161a8d6e33af8c3616f706e1
Malik 3 <malikkotb@icloud.com>
2025-01-11 15:27:23 +0100

    pixel image text reveal

--- app/pixelTextReveal/page.jsx
+++ app/pixelTextReveal/page.jsx
@@ -4,27 +4,37 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
 export default function Page() {
+  const [src, setSrc] = useState("/maskReveal/img5.png");
+
+  const [isHovered, setIsHovered] = useState(false);
+
   useGSAP(() => {
-    gsap.from(".borderr", {
+    gsap.from(".pixel", {
       opacity: 0,
-      duration: 1,
+      ease: "none",
+      stagger: { amount: 0.5, from: "random" },
+      // onComplete: () => { setSrc("/maskReveal/img1.png"); }
     });
   }, []);
 
   return (
     <div className="bg-black h-screen flex items-center justify-center">
-      <div className="relative h-[35vh] w-[35vh] bg-red-700 grid grid-cols-6 grid-rows-6">
-        <div className="absolute h-full w-full bg-blue-500">
+      <div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        className="relative h-[40vh] w-[40vh] bg-red-700 grid grid-cols-7 grid-rows-7"
+      >
+        <div className="absolute h-full w-full">
           <Image
-            src={"/maskReveal/img1.png"}
-            // src={"/maskReveal/img5.png"}
+            // src={"/maskReveal/img1.png"}
+            src={src}
             fill
             className="object-cover"
             alt=""
           />
         </div>
-        {Array.from({ length: 36 }).map((_, index) => (
-          <div key={index} className="bg-orange-400 z-10 opacity-0"></div>
+        {Array.from({ length: 49 }).map((_, index) => (
+          <div key={index} className="bg-orange-400 pixel z-10"></div>
         ))}
       </div>
     </div>
