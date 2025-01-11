fcf3745c0fb3c9edd916528d95aec8bc95d4f6f5
Malik 3 <malikkotb@icloud.com>
2025-01-11 16:45:44 +0100

    pixel image text reveal

--- app/pixelTextReveal/page.jsx
+++ app/pixelTextReveal/page.jsx
@@ -5,38 +5,59 @@ import { useGSAP } from "@gsap/react";
 import { useState } from "react";
 
 export default function Page() {
-  const [src, setSrc] = useState("/maskReveal/img5.png");
+  const images = ["/maskReveal/img1.png", "/maskReveal/img5.png"];
+  const getNextImage = (currentSrc) => {
+    const currentIndex = images.indexOf(currentSrc);
+    const nextIndex = (currentIndex + 1) % images.length;
+    return images[nextIndex];
+  };
+
+  const [src, setSrc] = useState(images[1]);
 
   const [isHovered, setIsHovered] = useState(false);
 
   useGSAP(() => {
     const tl = gsap.timeline();
 
-    tl.fromTo(
-      ".pixel",
-      { clipPath: "inset(50% 50% 50% 50%)" },
-      {
-        clipPath: "inset(-1% -1% -1% -1%)",
-        duration: 0.5,
-        ease: "power1.out",
-        stagger: { each: 0.005, from: "random" },
-        onComplete: () => {
-          setSrc("/maskReveal/img1.png");
-        },
-      }
-    );
-
-    // if (isHovered) {
-    //   tl.to(".pixel", {
-    //     clipPath: "inset(50% 50% 50% 50%)",
-    //     duration: 0.5,
-    //     ease: "power1.out",
-    //     stagger: { each: 0.005, from: "random" },
-    //     onComplete: () => {
-    //       setSrc("/maskReveal/img5.png");
-    //     },
-    //   });
-    // }
+    if (isHovered) {
+      tl.fromTo(
+        ".pixel",
+        { clipPath: "inset(50% 50% 50% 50%)" },
+        {
+          clipPath: "inset(-1% -1% -1% -1%)",
+          //   duration: 0.5,
+          ease: "power1.out",
+          stagger: { amount: 0.1, from: "random" },
+          onComplete: () => {
+            setSrc(images[0]);
+          },
+        }
+      ).to(".pixel", {
+        clipPath: "inset(50% 50% 50% 50%)",
+        // duration: 0.5,
+        ease: "power1.in",
+        stagger: { amount: 0.2, from: "random" },
+      });
+    } else {
+      tl.fromTo(
+        ".pixel",
+        { clipPath: "inset(50% 50% 50% 50%)" },
+        {
+          clipPath: "inset(-1% -1% -1% -1%)",
+          //   duration: 0.5,
+          ease: "power1.out",
+          stagger: { amount: 0.1, from: "random" },
+          onComplete: () => {
+            setSrc(images[1]);
+          },
+        }
+      ).to(".pixel", {
+        clipPath: "inset(50% 50% 50% 50%)",
+        // duration: 0.5,
+        ease: "power1.in",
+        stagger: { amount: 0.2, from: "random" },
+      });
+    }
   }, [isHovered]);
 
   return (
@@ -44,7 +65,7 @@ export default function Page() {
       <div
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
-        className="relative h-[40vh] w-[40vh] grid grid-cols-7 grid-rows-7"
+        className="relative h-[40vh] w-[40vh] grid grid-cols-6 grid-rows-6"
       >
         <div className="absolute h-full w-full">
           <Image
@@ -55,7 +76,7 @@ export default function Page() {
             alt=""
           />
         </div>
-        {Array.from({ length: 49 }).map((_, index) => (
+        {Array.from({ length: 36 }).map((_, index) => (
           <div
             key={index}
             style={{ clipPath: "inset(50% 50% 50% 50%)" }}
