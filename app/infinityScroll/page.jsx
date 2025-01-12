251e780b7497fc2146b371e2e62ad9adedde2fb8
Malik 3 <malikkotb@icloud.com>
2025-01-12 12:56:28 +0100

    infinity

--- app/infinityScroll/page.jsx
+++ app/infinityScroll/page.jsx
@@ -27,12 +27,12 @@ export default function InfinityScroll() {
 
   useEffect(() => {
     observerRef.current = new IntersectionObserver(handleIntersection, {
-      // threshold: 1,
+      threshold: 1,
       // rootMargin: allows us to offset when sth will happen
       // rootMargin: "-100px", // this says our container is now 100px smaller than it normally would be
       // => from the top of the container we're subtracting 100px, everything is leaving and entering 100px earlier
       // rootMargin: "100px", // all the animations are playing when the element is 100px away from becoming on the screen
-      
+
       // default root is the viewport (entire screen itself)
       //   root: // with root we can specify the container that we're watching
       // as long as you set root to some parent element, it will track all the children elements of that parent element
