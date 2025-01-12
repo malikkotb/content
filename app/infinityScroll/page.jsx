efbd0c478b6680a6cc7071e8e28e45abe4a3c5b0
Malik 3 <malikkotb@icloud.com>
2025-01-12 12:06:34 +0100

    infinte scroll

--- app/infinityScroll/page.jsx
+++ app/infinityScroll/page.jsx
@@ -1,15 +1,27 @@
 "use client";
 import gsap from "gsap";
 import styles from "./style.module.css";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useEffect, useRef } from "react";
 export default function InfinityScroll() {
+  // TODO: use only gsap for transitions (mixing them up with css, might fuck things up)
 
+  useEffect(() => {
+    const cards = document.querySelectorAll(".card");
 
-    // TODO: use only gsap for transitions (mixing them up with css, might fuck things up)
+    const observer = new IntersectionObserver((entries) => {
+      console.log(entries);
+    });
+
+    observer.observe(cards[0]);
+  }, []);
 
   return (
     <div className="mt-20 pl-8">
       <div className={styles.cardContainer}>
-        <div className={styles.card}>This is the first card.</div>
+        <div className={`${styles.card} ${styles.show}`}>
+          This is the first card.
+        </div>
         <div className={styles.card}>This is a card.</div>
         <div className={styles.card}>This is a card.</div>
         <div className={styles.card}>This is a card.</div>
