da423a2b9602803122723602ccd63f1894b1452a
Malik 3 <malikkotb@icloud.com>
2025-01-13 14:32:53 +0100

    zoom image scroll

--- app/zoomImageScroll/page.jsx
+++ app/zoomImageScroll/page.jsx
@@ -4,6 +4,7 @@ import { useRef, useEffect } from "react";
 import Lenis from "lenis";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { animationValues } from "./values.js";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
@@ -26,59 +27,6 @@ export default function Page() {
     }
   };
   const addToImageRefs = addToRefs(imageRefs);
-
-  const animationValues = [
-    {
-      src: "/drag/img1.png",
-      scale: 2,
-      x: 100,
-      y: 50,
-      top: 0,
-      left: 0,
-      start: "100px",
-      end: "500px",
-    },
-    {
-      src: "/drag/img2.png",
-      scale: 1.5,
-      x: -50,
-      y: 100,
-      top: 100,
-      left: 50,
-      start: "100px",
-      end: "500px",
-    },
-    {
-      src: "/drag/img3.png",
-      scale: 3,
-      x: 0,
-      y: -150,
-      top: 200,
-      left: 100,
-      start: "100px",
-      end: "500px",
-    },
-    {
-      src: "/drag/img4.png",
-      scale: 1.8,
-      x: 50,
-      y: 0,
-      top: 300,
-      left: 150,
-      start: "100px",
-      end: "500px",
-    },
-    {
-      src: "/drag/img5.png",
-      scale: 2.5,
-      x: -100,
-      y: -50,
-      top: 400,
-      left: 200,
-      start: "100px",
-      end: "500px",
-    },
-  ];
 
   useGSAP(() => {
     animationValues.forEach((values, index) => {
