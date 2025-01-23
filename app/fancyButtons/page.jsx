178a685412bf42e7d726c9cc6fe33c0a07685634
Malik 3 <malikkotb@icloud.com>
2025-01-23 10:36:40 +0100

    fancy buttons

--- app/fancyButtons/page.jsx
+++ app/fancyButtons/page.jsx
@@ -74,6 +74,64 @@ function FancyButton({ label, onClick }) {
     // if you want to see some more easings in action
     // and even create your own easing functions you can go to
     // https://easings.co/
+
+    // Apply to the first button with custom xDistance and stagger options
+    const blurButton1 = document.querySelector("#button1");
+    const iconRight1 = document.querySelector("#button1 .icon-right");
+    const iconLeft1 = document.querySelector("#button1 .icon-left");
+    const characters1 = document.querySelectorAll("#button1 .blur-span .char");
+
+    console.log(characters1);
+
+    createHoverAnimation(blurButton1, iconRight1, iconLeft1, characters1, 45, {
+      each: 0.04,
+    });
+
+    // Apply to the second button with custom xDistance and stagger options
+    const blurButton2 = document.querySelector("#button2");
+    const iconRight2 = document.querySelector("#button2 .icon-right-2");
+    const iconLeft2 = document.querySelector("#button2 .icon-left-2");
+    const characters2 = document.querySelectorAll("#button2 .blur-span .char");
+
+    createHoverAnimation(blurButton2, iconRight2, iconLeft2, characters2, -45, {
+      each: 0.04,
+      from: "end",
+    });
+
+    // Volume Button Hover Animation
+    const volumeBtn = document.querySelector(".volume-button");
+    const volumeBars = document.querySelectorAll(".volume-bars");
+
+    volumeBtn.addEventListener("mouseenter", () => {
+      volumeBars.forEach((bar) => {
+        bar.style.opacity = "1";
+      });
+
+      let hoverTimeline = gsap.timeline();
+      hoverTimeline
+        .to(
+          volumeBars,
+          {
+            filter: "blur(4px)", // Apply blur
+            duration: 0.25, // Animation duration for each character
+            y: -8,
+            stagger: 0.05, // Stagger effect
+            ease: "power3.out",
+          },
+          "<"
+        )
+        .to(
+          volumeBars,
+          {
+            filter: "blur(0px)", // Remove blur
+            duration: 0.25,
+            y: 0,
+            stagger: 0.05, // Stagger effect in reverse
+            ease: "power3.out",
+          },
+          "<=0.05"
+        );
+    });
   });
 
   // to reverse the animation when we stop hovering:
