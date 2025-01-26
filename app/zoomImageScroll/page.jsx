9faf145e3a66857d69a04e80e66d7eb1098aa184
Malik 3 <malikkotb@icloud.com>
2025-01-26 17:22:52 +0100

    zoom image Scroll

--- app/zoomImageScroll/page.jsx
+++ app/zoomImageScroll/page.jsx
@@ -30,7 +30,7 @@ export default function Page() {
     // GSAP ScrollTrigger for scaling effect
     imageRefs.current.forEach((image, index) => {
       gsap.to(image, {
-        scale: 2, // Final scale value (2x zoom)
+        scale: 4, // Final scale value (2x zoom)
         scrollTrigger: {
           trigger: containerRef.current,
           start: "top top", // Start when container top aligns with viewport top
@@ -42,30 +42,46 @@ export default function Page() {
   }, []);
 
   return (
-    <div ref={containerRef} className='bg-black relative h-[500vh]'>
-      <div className='sticky overflow-hidden top-0 h-[100vh]'>
-        {animationValues.map((src, index) => {
-          return (
-            <div
-              key={index}
-              ref={addToImageRefs}
-              className='absolute top-0 w-full h-full flex items-center justify-center'
-            >
-              <div
-                style={{
-                  width: `${src.width}vw`,
-                  height: `${src.height}vh`,
-                  top: `${src.top}vh`,
-                  left: `${src.left}vw`,
-                }}
-                className='relative'
-              >
-                <Image src={src} fill alt='image' className='object-cover' />
-              </div>
-            </div>
-          );
-        })}
+    <>
+      <div className='parent h-screen'>
+        <div className='div3'>3</div>
+        <div className='div4'>4</div>
+        <div className='div12'>12</div>
+        <div className='div13'>13</div>
+        <div className='div16'>16</div>
+        <div className='div17'>17</div>
+        <div className='div18'>18</div>
+        <div className='div19'>19</div>
+        <div className='div20'>20</div>
+        <div className='div21'>21</div>
+        <div className='div22'>22</div>
       </div>
-    </div>
+
+      {/* <div ref={containerRef} className='bg-black relative h-[500vh]'>
+        <div className='sticky overflow-hidden top-0 h-[100vh]'>
+          {animationValues.map((src, index) => {
+            return (
+              <div
+                key={index}
+                ref={addToImageRefs}
+                className='absolute top-0 w-full h-full flex items-center justify-center'
+              >
+                <div
+                  style={{
+                    width: `${src.width}vw`,
+                    height: `${src.height}vh`,
+                    top: `${src.top}vh`,
+                    left: `${src.left}vw`,
+                  }}
+                  className='relative'
+                >
+                  <Image src={src} fill alt='image' className='object-cover' />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div> */}
+    </>
   );
 }
