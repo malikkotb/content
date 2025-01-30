776a4a665f870d9e3cd00cbe13ffead59e67f455
Malik 3 <malikkotb@icloud.com>
2025-01-30 09:25:09 +0100

    zoom gallery scroll

--- app/zoomScrollGallery/page.jsx
+++ app/zoomScrollGallery/page.jsx
@@ -36,74 +36,51 @@ export default function Page() {
       //   console.log("index: ", index, values[index].scaleTo);
       // if (index >= 4) gsap.set(image, { visibility: "hidden" });
 
-      gsap.to(image, {
-        // scale: animationValues[index].scaleTo, // Final scale value
-        scale: 8,
-        visibility: "visible",
-        scrollTrigger: {
-          //   trigger: containerRef.current,
-          start: "top top", // Start when container top aligns with viewport top
-          //   start: `top+=${index < 4 ? 0 : 400}vh top`, // First images start at top, others start after 200vh
-          end: "bottom bottom", // End when container bottom aligns with viewport bottom
-          scrub: true, // Smoothly tie animation to scroll position
-          markers: true,
-        },
-      });
+      gsap.fromTo(
+        image,
+        { scale: values[index].scaleFrom },
+        {
+          scale: values[index].scaleTo, // Final scale value
+          //   scale: 5,
+          scrollTrigger: {
+            //   trigger: containerRef.current,
+            start: "top top", // Start when container top aligns with viewport top
+            // TODO: make the start activate every 5 elements
+            // so that 5 elements start simultanously
+            // start: `top+=${index < 5 ? 0 : 200}vh top`, // First images start at top, others start after 200vh
+            end: "bottom bottom", // End when container bottom aligns with viewport bottom
+            scrub: true, // Smoothly tie animation to scroll position
+            markers: true,
+          },
+        }
+      );
     });
   }, []);
 
   return (
     <>
-      <div className='h-[300vh] overflow-hidden'>
-        {values.map((element, index) => (
-          <div
-            key={index}
-            ref={addToImageRefs}
-            className='absolute top-0 w-full h-full flex items-center justify-center'
-          >
+      <div className='h-[700vh] overflow-hidden'>
+        <div className='h-screen sticky overflow-hidden top-0'>
+          {values.map((element, index) => (
             <div
-              style={{
-                // width: `${src.width}vw`,
-                // height: `${src.height}vh`,
-                // top: `${element.top}vh`,
-                left: `${element.left}vw`,
-              }}
-              className='bg-green-500 relative h-44 w-52'
-              // TODO: explain that the most important values here are relative (so relative to the parent container which is an absolute container)
-              // and then the top and left values for positioning
-            ></div>
-          </div>
-        ))}
-        {/* <div
-          ref={addToImageRefs}
-          className='absolute borderr top-0 w-full h-full flex items-center justify-center '
-        >
-          <div className='bg-green-500 relative h-44 w-52 -left-[40vw]'></div>
-        </div> */}
-        {/* <div
-          ref={addToImageRefs}
-          className='absolute top-0 w-full h-full flex items-center justify-center'
-        >
-          <div className='bg-red-500 relative h-44 w-52 -left-[20vw]'></div>
+              key={index}
+              ref={addToImageRefs}
+              className='absolute top-0 w-full h-full flex items-center justify-center'
+            >
+              <div
+                style={{
+                  // width: `${src.width}vw`,
+                  // height: `${src.height}vh`,
+                  top: `${element.top}vh`,
+                  left: `${element.left}vw`,
+                }}
+                className='bg-green-500 relative h-44 w-52'
+                // TODO: explain that the most important values here are relative (so relative to the parent container which is an absolute container)
+                // and then the top and left values for positioning
+              ></div>
+            </div>
+          ))}
         </div>
-        <div
-          ref={addToImageRefs}
-          className='absolute top-0 w-full h-full flex items-center justify-center'
-        >
-          <div className='bg-orange-500 relative h-44 w-52'></div>
-        </div>
-        <div
-          ref={addToImageRefs}
-          className='absolute top-0 w-full h-full flex items-center justify-center'
-        >
-          <div className='bg-yellow-500 relative left-[20vw] h-44 w-52'></div>
-        </div>
-        <div
-          ref={addToImageRefs}
-          className='absolute top-0 w-full h-full flex items-center justify-center '
-        >
-          <div className='bg-purple-500 relative left-[40vw] h-44 w-52'></div>
-        </div> */}
       </div>
     </>
   );
