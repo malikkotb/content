ae58549d8628f21ff8f85c111051fe028bf33d8b
Malik 3 <malikkotb@icloud.com>
2025-02-05 15:19:24 +0100

    mask section hero

---
+++ app/maskSectionHero/page.jsx
@@ -0,0 +1,22 @@
+"use client";
+import Image from "next/image";
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Page() {
+  const imageRefs = useRef([]);
+  const addToRefs = (refArray) => (el) => {
+    if (el && !refArray.current.includes(el)) {
+      refArray.current.push(el);
+    }
+  };
+  
+
+  useGSAP(() => {}, []);
+
+  return <></>;
+}
