b0dfcdffc684fee5e41ffaec3683266cb910d6f6
Malik 3 <malikkotb@icloud.com>
2025-02-05 15:39:06 +0100

    mask section hero

--- app/maskSectionHero/page.jsx
+++ app/maskSectionHero/page.jsx
@@ -7,16 +7,35 @@ import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger);
 
+// inspiration: mai sai gon
+
 export default function Page() {
+  const headingRef = useRef(null);
   const imageRefs = useRef([]);
   const addToRefs = (refArray) => (el) => {
     if (el && !refArray.current.includes(el)) {
       refArray.current.push(el);
     }
   };
-  
 
   useGSAP(() => {}, []);
 
-  return <></>;
+  return (
+    <div className='bg-black h-screen relative justify-center flex items-center'>
+      <div
+        style={{ backgroundImage: "url('/drag/img5.png')" }}
+        className='w-full h-full bg-cover bg-center'
+      ></div>
+
+      <div
+        ref={headingRef}
+        style={{ zIndex: 100 }}
+        className='absolute w-full px-6 leading-none font-serif text-white text-[150px] flex justify-between'
+      >
+        <h1>Mai</h1>
+        <h1>Sai</h1>
+        <h1>Gon</h1>
+      </div>
+    </div>
+  );
 }
