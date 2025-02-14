115b7a477ab3714a325f680b369df53a90864969
Malik 3 <malikkotb@icloud.com>
2025-02-14 10:18:52 +0100

    drag mobile

--- app/dragMobile/page.jsx
+++ app/dragMobile/page.jsx
@@ -43,77 +43,74 @@ export default function Home() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1 }}
-          className={`text-[53px] ${neueMontreal.className}`}
+          className={`text-[53px] text-left ${neueMontreal.className}`}
         >
-          You’ll also learn the little things people forget to teach
-          (that’ll make you look French) – like putting a space before
-          punctuation, writing your 1 like ^, and saying euh instead
-          of um.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Ipsam sed repellat minima tempora, neque eius quis eligendi
+          molestias, aut distinctio labor iosam.
         </motion.div>
       </div>
-      <div className='h-screen relative bg-black overflow-hidden font-semibold'>
-        <section
-          ref={containerRef}
-          style={{ zIndex: 1 }}
-          className='h-screen overflow-hidden w-full'
+      <section
+        ref={containerRef}
+        style={{ zIndex: 1 }}
+        className='h-[90vh] bg-black relative overflow-hidden w-full'
+      >
+        <div
+          ref={addToImageRefs}
+          className='w-[35vw] h-[30vw] absolute top-8 left-40 '
         >
-          <div
-            ref={addToImageRefs}
-            className='w-[35vw] h-[30vw] top-4 left-44 relative'
-          >
-            <Image
-              src='/dragMobile/1.gif'
-              fill
-              className='object-cover'
-              alt='1'
-            />
-          </div>
-          <div
-            ref={addToImageRefs}
-            className='w-[25vw] h-[25vw] absolute top-[60vh] left-[80vw]'
-          >
-            <Image
-              src='/dragMobile/3.gif'
-              fill
-              className='object-cover'
-              alt='1'
-            />
-          </div>
-          <div
-            ref={addToImageRefs}
-            className='w-[25vw] h-[25vw] absolute top-[60vh] left-[5vw]'
-          >
-            <Image
-              src='/dragMobile/4.gif'
-              fill
-              className='object-cover'
-              alt='1'
-            />
-          </div>
-          <div
-            ref={addToImageRefs}
-            className='w-[30vw] h-[30vw] absolute top-[60vh] left-[40vw]'
-          >
-            <Image
-              src='/dragMobile/5.gif'
-              fill
-              className='object-cover'
-              alt='1'
-            />
-          </div>
-          <div
-            ref={addToImageRefs}
-            className='w-[25vw] h-[35vw] absolute top-[25vh] left-[60vw]'
-          >
-            <Image
-              src='/dragMobile/2.gif'
-              fill
-              className='object-cover'
-              alt='1'
-            />
-          </div>
-        </section>
-      </div>
+          <Image
+            src='/dragMobile/1.gif'
+            fill
+            className='object-cover'
+            alt='1'
+          />
+        </div>
+        <div
+          ref={addToImageRefs}
+          className='w-[25vw] h-[25vw] absolute top-[40vh] left-[50vw]'
+        >
+          <Image
+            src='/dragMobile/3.gif'
+            fill
+            className='object-cover'
+            alt='1'
+          />
+        </div>
+        <div
+          ref={addToImageRefs}
+          className='w-[25vw] h-[25vw] absolute top-[60vh] left-[5vw]'
+        >
+          <Image
+            src='/dragMobile/4.gif'
+            fill
+            className='object-cover'
+            alt='1'
+          />
+        </div>
+        <div
+          ref={addToImageRefs}
+          className='w-[30vw] h-[30vw] absolute top-[60vh] left-[40vw]'
+        >
+          <Image
+            src='/dragMobile/5.gif'
+            fill
+            className='object-cover'
+            alt='1'
+          />
+        </div>
+        <div
+          ref={addToImageRefs}
+          className='w-[25vw] h-[35vw] absolute top-[25vh] left-[60vw]'
+        >
+          <Image
+            src='/dragMobile/2.gif'
+            fill
+            className='object-cover'
+            alt='1'
+          />
+        </div>
+      </section>
     </>
   );
 }
